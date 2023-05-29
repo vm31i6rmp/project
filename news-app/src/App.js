36fd70b7db082e4135309d9272ae6e25b9c92794
vm31i6rmp <vm31i6rmp@gmail.com>
2023-05-29 20:40:30 +0900

    update_0529

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -17,10 +17,19 @@ function App() {
     setResult("search?q=天気")
   }
   const handleResult2 = () => {
-    setResult("search?q=sports")
+    setResult("search?q=business")
   }
   const handleResult3 = () => {
+    setResult("search?q=sport")
+  }
+  const handleResult4 = () => {
     setResult("search?q=政治")
+  }
+  const handleResult5 = () => {
+    setResult("search?q=社会")
+  }
+  const handleResult6 = () => {
+    setResult("search?q=文化")
   }
 
 
@@ -29,11 +38,18 @@ function App() {
       <div className='logo'>
         <a href='http://xd195459.html.xdomain.jp/react-news/'><img src={iconImage} alt='H-news' /></a>
       </div>
-      <input type='text' ref={resultRef} />
-      <button onClick={handleResult}>検索</button>
-      <button onClick={handleResult1}>天気</button>
-      <button onClick={handleResult2}>スポーツ</button>
-      <button onClick={handleResult3}>政治</button>
+      <div className='btn-container'>
+        <input type='text' placeholder='キーワード入力' ref={resultRef} />
+        <button className='btn-research' onClick={handleResult}>検索</button>
+        <div className='btn-category-container'>
+          <button className='btn-category' onClick={handleResult1}>天気</button>
+          <button className='btn-category' onClick={handleResult2}>ビジネス</button>
+          <button className='btn-category' onClick={handleResult3}>スポーツ</button>
+          <button className='btn-category' onClick={handleResult4}>政治</button>
+          <button className='btn-category' onClick={handleResult5}>社会</button>
+          <button className='btn-category' onClick={handleResult6}>文化</button>
+        </div>
+      </div>
       <NewsList result={result}/>
     </div>
   );
