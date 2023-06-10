1916023c46f4f0ee72976fcf9b2d79da29f930a6
vm31i6rmp <vm31i6rmp@gmail.com>
2023-06-10 16:13:13 +0900

    update_0610

--- portfolio/main.js
+++ portfolio/main.js
@@ -15,3 +15,17 @@ var indexLink = document.querySelectorAll('a[href^="#"]');
       });
     });
   }
+
+//scroll_effect
+var scrollAnimationElms = document.querySelectorAll(
+  '.scroll_up, .scroll_left, .scroll_right, .scroll_center,.scroll_rotateX_left, .scroll_rotateX_right, .flip-scale-2-ver-left, .flip-scale-2-ver-right');
+var scrollAnimationFunc = function () {
+  for (var i = 0; i < scrollAnimationElms.length; i++) {
+    var triggerMargin = 100;
+    if (window.innerHeight > scrollAnimationElms[i].getBoundingClientRect().top + triggerMargin) {
+      scrollAnimationElms[i].classList.add('on');
+    }
+  }
+}
+window.addEventListener('load', scrollAnimationFunc);
+window.addEventListener('scroll', scrollAnimationFunc);
